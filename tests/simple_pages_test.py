c938623a13bb8468f49fa40ebf5330bd2fe19099
Diana Zawislak <97991555+DianaZawislak@users.noreply.github.com>
2022-02-26 19:31:14 -0500

    updated gallery

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,12 +1,9 @@
-"""This test the homepage"""
-
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Gallery</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Contact</a>' in response.data
+    assert b'<a class="nav-link" href="/">About</a>' in response.data
+    assert b'<a class="nav-link" href="/">Gallery</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
